40a1bea1529be8b6bcf367816e83444634deed61
ANURAG PANDEY <54503498+anurag2002@users.noreply.github.com>
2022-06-08 09:05:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,19 +36,20 @@ app.get("/", (req, res) => res.render("index"));
 // app.set('views', path.join(__dirname, ''));
 
 
-app.get("/register", (req, res) => res.sendFile(__dirname + "/views/registration_page.html"));
-app.get('/home', (req, res) => res.sendFile(__dirname + "home"));
-app.get("/1984", (req, res) => res.sendFile(__dirname + "/views/1984.html"));
-app.get("/chamber_of_secret", (req, res) => res.sendFile(__dirname + "/views/chamber_of_secret.html"));
-app.get("/diary_of_a_young_girl", (req, res) => res.sendFile(__dirname + "/views/diary_of_a_young_girl.html"));
-app.get("/dune", (req, res) => res.sendFile(__dirname + "/views/dune.html"));
-app.get("/eclipse_review", (req, res) => res.sendFile(__dirname + "/views/eclipse_review.html"));
-app.get("/harrypotter_sorcerer_stone", (req, res) => res.sendFile(__dirname + "/views/harrypotter_sorcerer_stone.html"));
-app.get("/list_of_books", (req, res) => res.sendFile(__dirname + "/views/list_of_books.html"));
-app.get("/order_of_phoneix", (req, res) => res.sendFile(__dirname + "/views/order_of_phoneix.html"));
-app.get("/philosopher's_stone", (req, res) => res.render("sendFile__dirname + '/views/s_stone.html"));
-app.get("/relativity", (req, res) => res.sendFile(__dirname + "/views/relativity.html"));
-app.get("/twilight_review", (req, res) => res.sendFile(__dirname + "/views/twilight_review.html"));
+app.get("/register", (req, res) => res.sendFile(__dirname + "/registration_page.html"));
+app.get('/home', (req, res) => res.sendFile(__dirname + "/home.html"));
+app.get("/1984", (req, res) => res.sendFile(__dirname + "/1984.html"));
+app.get("/chamber_of_secret", (req, res) => res.sendFile(__dirname + "/chamber_of_secret.html"));
+app.get("/diary_of_a_young_girl", (req, res) => res.sendFile(__dirname + "/diary_of_a_young_girl.html"));
+app.get("/dune", (req, res) => res.sendFile(__dirname + "/dune.html"));
+app.get("/eclipse_review", (req, res) => res.sendFile(__dirname + "/eclipse_review.html"));
+app.get("/harrypotter_sorcerer_stone", (req, res) => res.sendFile(__dirname + "/harrypotter_sorcerer_stone.html"));
+app.get("/list_of_books", (req, res) => res.sendFile(__dirname + "/list_of_books.html"));
+app.get("/order_of_phoneix", (req, res) => res.sendFile(__dirname + "/order_of_phoneix.html"));
+app.get("/philosopher's_stone", (req, res) => res.render("sendFile__dirname + '/s_stone.html"));
+app.get("/relativity", (req, res) => res.sendFile(__dirname + "/relativity.html"));
+app.get("/twilight_review", (req, res) => res.sendFile(__dirname + "/twilight_review.html"));
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
